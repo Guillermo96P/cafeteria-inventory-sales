5b9c5fbd8c36ea2b645e8a221547ea5fac8eee05
Guille <guille@MBP-de-Guille.bbrouter>
2024-12-05 11:00:57 -0500

    Se agrega estilos para el header y los botones (modales)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import SaleForm from "./components/SaleForm";
 import ProductForm from "./components/ProductForm";
 import Modal from "react-modal"; // Importar la biblioteca de modales
 import axios from "axios";
+import './App.css'; // Importar estilos
 
 /**
  * Componente principal de la aplicación
@@ -71,58 +72,64 @@ const App = () => {
   };
 
   return (
-    <div>
-      <h1>Gestión de Inventario y Ventas</h1>
+    <div className="app-container">
+      <header className="header">
+        <h1>Gestión de Inventario y Ventas</h1>
+      </header>
+      <main>
+        <div className="button-container">
+          <button className="btn btn-primary" onClick={openProductFormModal}>
+            Agregar Producto
+          </button>
+          <button className="btn btn-secondary" onClick={openSaleFormModal}>
+            Realizar Venta
+          </button>
+        </div>
 
-      <button onClick={openProductFormModal}>Agregar Producto</button>
-      <button onClick={openSaleFormModal}>Realizar Venta</button>
+        <Modal isOpen={isProductFormOpen} onRequestClose={closeProductFormModal}>
+          <ProductForm closeModal={closeProductFormModal} onProductAdded={fetchProducts} />
+        </Modal>
 
-      {/* Modal para agregar producto */}
-      <Modal isOpen={isProductFormOpen} onRequestClose={closeProductFormModal}>
-        <ProductForm closeModal={closeProductFormModal} onProductAdded={fetchProducts} />
-      </Modal>
+        <Modal isOpen={isSaleFormOpen} onRequestClose={closeSaleFormModal}>
+          <SaleForm closeModal={closeSaleFormModal} products={products} onSaleCompleted={fetchSales} />
+        </Modal>
 
-      {/* Modal para realizar venta */}
-      <Modal isOpen={isSaleFormOpen} onRequestClose={closeSaleFormModal}>
-        <SaleForm closeModal={closeSaleFormModal} products={products} onSaleCompleted={fetchSales} />
-      </Modal>
+        <ProductList products={products} />
 
-      {/* Lista de productos */}
-      <ProductList products={products} />
-
-      {/* Tabla del historial de ventas */}
-      <h2>Historial de Ventas</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Número de Venta</th>
-            <th>Nombre del Producto</th>
-            <th>Cantidad</th>
-            <th>Precio Total</th>
-            <th>Fecha de Venta</th>
-          </tr>
-        </thead>
-        <tbody>
-          {sales.map((sale) => (
-            <tr key={sale._id}>
-              <td>{sale._id}</td>
-              <td>{sale.productId ? sale.productId.name : "Producto no disponible"}</td>
-              <td>{sale.quantity}</td>
-              <td>{sale.productId ? sale.productId.price * sale.quantity : "N/A"}</td>
-              <td>{new Date(sale.createdAt).toLocaleString('es-ES', { 
-                year: 'numeric', 
-                month: '2-digit', 
-                day: '2-digit', 
-                hour: '2-digit', 
-                minute: '2-digit', 
-                second: '2-digit', 
-                hour12: false 
-                })}
-              </td>
-          </tr>
-          ))}
-        </tbody>
-      </table>
+        <h2>Historial de Ventas</h2>
+        <table className="sales-table">
+          <thead>
+            <tr>
+              <th>Número de Venta</th>
+              <th>Nombre del Producto</th>
+              <th>Cantidad</th>
+              <th>Precio Total</th>
+              <th>Fecha de Venta</th>
+            </tr>
+          </thead>
+          <tbody>
+            {sales.map((sale) => (
+              <tr key={sale._id}>
+                <td>{sale._id}</td>
+                <td>{sale.productId ? sale.productId.name : "Producto no disponible"}</td>
+                <td>{sale.quantity}</td>
+                <td>{sale.productId ? sale.productId.price * sale.quantity : "N/A"}</td>
+                <td>
+                  {new Date(sale.createdAt).toLocaleString('es-ES', {
+                    year: 'numeric',
+                    month: '2-digit',
+                    day: '2-digit',
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    second: '2-digit',
+                    hour12: false
+                  })}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </main>
     </div>
   );
 };
